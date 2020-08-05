2c5303abd0d768db2f255e5b07906f63c529d04d
sem.langhi@icloud.com <samuele.langhi@mail.polimi.it>
2020-08-05 19:34:37 +0300

    fixing commit

--- ICEP/src/main/java/ee/ut/cs/dsg/example/linearroad/mapper/SpeedMapper.java
+++ ICEP/src/main/java/ee/ut/cs/dsg/example/linearroad/mapper/SpeedMapper.java
@@ -58,8 +58,8 @@ public class SpeedMapper extends RichMapFunction<String, SpeedEvent> {
             registerCounter++;
             performanceFileBuilder.register(query, experimentId, startTime, currentTime, eventsCounter, implementation, parallelism);
         }
-        if(System.currentTimeMillis()-startTime>30000)
-            performanceFileBuilder.register(query, experimentId, startTime, System.currentTimeMillis(), eventsCounter, implementation, parallelism);
+       // if(System.currentTimeMillis()-startTime>30000)
+       //     performanceFileBuilder.register(query, experimentId, startTime, System.currentTimeMillis(), eventsCounter, implementation, parallelism);
 
         return new SpeedEvent(data[0].trim(),Long.parseLong(data[8].trim()),Double.parseDouble(data[1].trim()));
 
